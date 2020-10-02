e59fa04879c9927bd4d601b12535cefc39804114
Rameez071 <rameezraja405@gmail.com>
2020-10-02 17:03:13 +0530

    Add : Employee Wage calculation code added

--- EmpolyeeWageComputationProblem/src/EmployeeWage/EmployeeWageComputation.java
+++ EmpolyeeWageComputationProblem/src/EmployeeWage/EmployeeWageComputation.java
@@ -9,12 +9,21 @@ public class EmployeeWageComputation {
 	}
 
 }
-class checkAttendance {
+class checkAttendance extends calculateWage{
     double check = Math.floor(Math.random() * 10) % 2;
     void isPresent(){
-        if ( check == 1)
+        if ( check == 1) {
             System.out.println("Employee is Present !");
+            System.out.println("Wage is "+ dailyWage());
+        }
         else
             System.out.println("Employee is Absent !");
+    }
+}
+class calculateWage {
+    final int wagePerHr = 20;
+    final int workHr = 8;
+    int dailyWage(){
+        return wagePerHr * workHr;
     }
 }
