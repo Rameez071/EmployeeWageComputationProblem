dfe451a6df8aae5c231990191a387c5ec2529e36
Rameez071 <rameezraja405@gmail.com>
2020-10-02 16:43:01 +0530

    Add : Welcome message added

---
+++ EmpolyeeWageComputationProblem/src/EmployeeWage/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package EmployeeWage;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee wage computation problem");
+
+	}
+
+}
