157c378a6138b84e29aa41ff2e6538ee697488f3
Rameez071 <rameezraja405@gmail.com>
2020-10-02 16:55:03 +0530

    Add : Employee Attendance Check added

--- EmpolyeeWageComputationProblem/src/EmployeeWage/EmployeeWageComputation.java
+++ EmpolyeeWageComputationProblem/src/EmployeeWage/EmployeeWageComputation.java
@@ -3,8 +3,18 @@ package EmployeeWage;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
+		checkAttendance employee = new checkAttendance();
 		System.out.println("Welcome to Employee wage computation problem");
-
+		employee.isPresent();
 	}
 
+}
+class checkAttendance {
+    double check = Math.floor(Math.random() * 10) % 2;
+    void isPresent(){
+        if ( check == 1)
+            System.out.println("Employee is Present !");
+        else
+            System.out.println("Employee is Absent !");
+    }
 }
